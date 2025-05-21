7173f40db51d1fa0612daeb45314dfa9f7d0d45a
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2025-05-21 11:54:32 -0600

    Create Layout

---
+++ src/views/Contact.tsx
@@ -0,0 +1,12 @@
+// pages/contact.tsx
+import Layout from "../layouts/Layouts"; // ajusta la ruta si es necesario
+
+export default function Contact() {
+  return (
+    <>
+        <h2 className="text-2xl font-semibold mb-4">Contáctanos</h2>
+        <p>Estamos aquí para ayudarte. Puedes escribirnos al correo contacto@misitio.com.</p>
+    </>
+    
+  );
+}
