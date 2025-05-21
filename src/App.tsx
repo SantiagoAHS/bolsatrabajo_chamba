bf57d23409ca216c7ab3e4f935fe6db1e216c3db
SuperExe404 <diegomon0440@gmail.com>
2025-05-21 11:19:08 -0600

    Intalation of Tailwind 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <h1 className="font-extrabold text-2xl">Hola Mundo</h1>
       <p>Esta es una aplicación de prueba</p>
     </div>
   );
