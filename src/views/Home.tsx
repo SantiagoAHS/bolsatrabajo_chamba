dba3cd040db69a5fbc858cbb5551a5ebbc58fb1d
DiegoARomeroH <diegoalonsoromerohernndez@gmail.com>
2025-05-21 17:26:42 -0600

    Rework from Home Page

---
+++ src/views/Home.tsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+
+      return (
+        <>
+        <h1 className="text-6xl font-extrabold">👨🏾‍🔧Chambas👨🏾‍🔧</h1>
+        <p>Acá chambeas o te hacemos chambear</p>
+        </>
+        );
+}
