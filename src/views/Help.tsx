c955b77ab26a4bc9a141ce11907876eb7233b2b5
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2025-05-26 12:50:28 -0600

    Create contact

---
+++ src/views/Help.tsx
@@ -0,0 +1,11 @@
+import Layout from "../layouts/Layouts";
+
+export default function Help() {
+  return (
+    <>
+        <h2 className="text-2xl font-semibold mb-4">Ayuda</h2>
+        <p>Alguna ayuda aqui estan las preguntas frecuentes</p>
+    </>
+    
+  );
+}
