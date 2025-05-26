6bece293a60bfc69b1572e5b53bd134cf445cbf1
SuperExe404 <diegomon0440@gmail.com>
2025-05-26 12:50:56 -0600

    About

---
+++ src/views/About.tsx
@@ -0,0 +1,11 @@
+import Layout from "../layouts/Layouts";
+
+export default function About() {
+  return (
+    <>
+        <h2 className="text-2xl font-semibold mb-4">About Us</h2>
+        <p>Yanet is property of Dealer</p>
+    </>
+    
+  );
+}
